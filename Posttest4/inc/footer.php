40bab8cc67a02f7d19c548b5b50c15210be15e44
Fajrin17 <145562787+Fajrin17@users.noreply.github.com>
2023-10-11 19:39:38 +0800

    Delete Posttest4 directory

--- Posttest4/inc/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <p class="left">
-        Ayo cari informasi ikan hias favorit kalian di Aquafish Nusantara,
-        karena hanya di Aquafish Nusantara setiap ikan hias dijelaskan secara
-        lengkap dan detail!
-    </p>
-    <p class="right">Hak Cipta © 2023 All Rights Reserved</p>
-</footer>
--- Posttest4/js/script-about.js
+++
@@ -1,7 +0,0 @@
-var tombol = document.getElementById('toggle-mode');
-tombol.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-});
-
-const title = document.querySelector('.left');
-title.style.color = '#FFD1DA';
